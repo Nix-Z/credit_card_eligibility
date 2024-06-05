dd9da9e307bbcd541336b99bd1b67422a67a3c41
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-06-05 14:40:08 -0700

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,7 +4,7 @@ from data_analysis import analyze_data
 def preprocess_data():
   data, categorical_features, numerical_features = analyze_data()
   
-  data.drop(['ID', 'Num_children', 'Income_type', 'Work_phone'], axis=1, inplace=True)
+  data.drop(['ID', 'Num_children', 'Income_type', 'Work_phone', 'Email'], axis=1, inplace=True)
   data['Age'] = data['Age'].astype(int)
   data['Total_income'] = data['Total_income'].astype(int)
   data['Years_employed'] = data['Years_employed'].astype(int)
