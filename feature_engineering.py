9e4b6918fe819891f571ffed43c599c3b29ff550
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-06-05 14:11:01 -0700

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -20,22 +20,27 @@ def engineer_features():
         label_encoders[categorical_feature] = le  # Store the label encoder if needed later
         unique_values = le.classes_
         print(f"Unique values in {categorical_feature}: {unique_values}")
-
     
+    # If use case is used for clustering, drop Target column
+    data.drop(['Target'], axis=1, inplace=True)
+
+    # If use case is used for classification, balance Target classes
+    '''
     # Upsample the minority class to address class imbalance
     data_majority = data[data['Target'] == 0]
     data_minority = data[data['Target'] == 1]
     data_minority_upsampled = resample(data_minority, replace=True, n_samples=len(data_majority), random_state=1)
-    data_upsampled = pd.concat([data_majority, data_minority_upsampled], axis=0)
+    data = pd.concat([data_majority, data_minority_upsampled], axis=0)
 
     # Display the class distribution after upsampling
     print("Class distribution after upsampling:")
-    print(data_upsampled['Target'].value_counts())
-
-    print(data_upsampled.head())
-
-    data_upsampled.to_csv('credit_card_eligibility_cleansed_data.csv', index=False)
+    print(data['Target'].value_counts())
+    '''
     
-    return data_upsampled
+    print(data.head())
+
+    data.to_csv('credit_card_eligibility_cleansed_data.csv', index=False)
+    
+    return data
 
 engineer_features()
